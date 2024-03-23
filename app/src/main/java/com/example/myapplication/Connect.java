7a5be2e06226985f59bad324ef8ebc08aa55ed07
kitashima_desktop <110659558+kitashimauni@users.noreply.github.com>
2024-03-24 01:07:43 +0900

    init

---
+++ app/src/main/java/com/example/myapplication/Connect.java
@@ -0,0 +1,96 @@
+package com.example.myapplication;
+
+import android.Manifest;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Queue;
+import java.util.UUID;
+
+public class Connect extends Thread {
+    private final BluetoothSocket mmSocket;
+    private final BluetoothDevice mmDevice;
+    private final MainActivity mainActivity;
+    private final Queue<Byte> que;
+    public Connect(MainActivity mainActivity, BluetoothDevice device, UUID uuid) {
+        BluetoothSocket tmp = null;
+        mmDevice = device;
+        this.mainActivity = mainActivity;
+        que = mainActivity.getQue();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            if (ActivityCompat.checkSelfPermission(mainActivity, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                Toast toast = Toast.makeText(mainActivity.getBaseContext(), "Bluetoothの接続を許可してください", Toast.LENGTH_SHORT);
+                toast.show();
+                mainActivity.requestPermissions(new String[]{Manifest.permission.BLUETOOTH_CONNECT}, 0);
+            }
+        }
+
+        try {
+            tmp = device.createRfcommSocketToServiceRecord(uuid);
+        } catch (IOException e) {
+            Log.e("Bluetooth Error", e + " in " + this);
+        }
+
+        mmSocket = tmp;
+    }
+
+    @Override
+    public void run() {
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                if (ActivityCompat.checkSelfPermission(mainActivity, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                    Toast toast = Toast.makeText(mainActivity.getBaseContext(), "Bluetoothの接続を許可してください", Toast.LENGTH_SHORT);
+                    toast.show();
+                    mainActivity.requestPermissions(new String[]{Manifest.permission.BLUETOOTH_CONNECT}, 0);
+                }
+            }
+            Log.d("From", "ここから接続開始");
+            mmSocket.connect();
+        } catch (IOException connectException) {
+            try {
+                Log.e("Bluetooth Error", connectException + " in " + this);
+                mmSocket.close();
+            } catch (IOException closeException) {
+                Log.e("Bluetooth Error", closeException + " in " + this);
+            }
+        }
+
+        try{
+            OutputStream outputStream = mmSocket.getOutputStream();
+            while(true){
+                if(que.size() > 0){
+                    Byte data = que.poll();
+                    if(data == null) continue;
+                    if(data == 0x0) break;
+                    byte[] command = new byte[]{data};
+                    outputStream.write(command);
+                }
+            }
+        } catch (IOException e){
+            Log.e("OutputStream Error", e + " in " + this);
+        }
+
+        try {
+            mmSocket.close();
+        } catch (IOException closeException) {
+            Log.e("Bluetooth Error", closeException + " in " + this);
+        }
+    }
+
+    public void cancel() {
+        try {
+            mmSocket.close();
+        } catch (IOException closeException) {
+            Log.e("Bluetooth Error", closeException + " in " + this);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,141 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothProfile;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.ParcelUuid;
+import android.os.PatternMatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Set;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final byte leftKey = (byte) 0x25;
+    private static final byte rightKey = (byte) 0x27;
+    private static final byte media_play_pause = (byte) 0xB3;
+    private static final byte media_prev = (byte) 0xB1;
+    private static final byte media_next = (byte) 0xB0;
+    private Queue<Byte> que;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+//            if (ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+//                    != PackageManager.PERMISSION_GRANTED) {
+//                ActivityCompat.requestPermissions(
+//                        MainActivity.this,
+//                        new String[]{Manifest.permission.ACCESS_BACKGROUND_LOCATION},
+//                        0);
+//            }
+//        }
+
+        que = new ArrayDeque<>();
+
+        View button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                if (!bluetoothAdapter.isEnabled()) {
+                    Toast toast = Toast.makeText(getBaseContext(), "Bluetoothをオンにしてください", Toast.LENGTH_SHORT);
+                    toast.show();
+                    return;
+                }
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    if (ActivityCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                        Toast toast = Toast.makeText(getBaseContext(), "Bluetoothの接続を許可してください", Toast.LENGTH_SHORT);
+                        toast.show();
+                        requestPermissions(new String[]{Manifest.permission.BLUETOOTH_CONNECT}, 0);
+                    }
+                }
+                Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+                if (pairedDevices.size() > 0) {
+                    for (BluetoothDevice device : pairedDevices) {
+                        String name = device.getName();
+                        String mac = device.getAddress();
+                        Log.d("Names", name + ": " + mac);
+                    }
+                }
+
+                // String mac_address = "C8:2A:DD:AC:1D:62";
+                String mac_address = "08:BE:AC:3E:2A:9A";
+                UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+                // UUID uuid = UUID.nameUUIDFromBytes(new byte[] {(byte) 0x1124});
+                BluetoothDevice bluetoothDevice = bluetoothAdapter.getRemoteDevice(mac_address);
+                Log.d("Discovering", "is " + bluetoothAdapter.isDiscovering());
+
+                Thread thread = new Thread(new Connect(MainActivity.this, bluetoothDevice, uuid));
+                thread.start();
+            }
+        });
+
+        Button leftButton = findViewById(R.id.left_button);
+        leftButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                que.add(leftKey);
+            }
+        });
+
+        Button rightButton = findViewById(R.id.right_button);
+        rightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                que.add(rightKey);
+            }
+        });
+
+        Button mediaPlayPauseButton = findViewById(R.id.media_play_pause_button);
+        mediaPlayPauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                que.add(media_play_pause);
+            }
+        });
+
+        Button mediaPrevButton = findViewById(R.id.media_prev_button);
+        mediaPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                que.add(media_prev);
+            }
+        });
+
+        Button mediaNextButton = findViewById(R.id.media_next_button);
+        mediaNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                que.add(media_next);
+            }
+        });
+    }
+
+    public Queue<Byte> getQue() {
+        return que;
+    }
+}
